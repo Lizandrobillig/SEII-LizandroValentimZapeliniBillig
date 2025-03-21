b28ff3d799d1a4a1b4de2543b9583ed1427383ff
Lizandrobillig <lizandrozbillig01@gmail.com>
2025-03-21 20:26:18 -0300

    first commit

---
+++ Exercicio02/Video10/08GetSet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08GetSet
+{
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            
+            Person p = new Person();
+            p.Name = "Lizandro";
+            Console.WriteLine(p.Name);
+
+        }
+    }
+}
