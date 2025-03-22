75a8575fdd1f85801947e474e6dd0303e793564a
Lizandrobillig <lizandrozbillig01@gmail.com>
2025-03-22 16:18:49 -0300

    first commit

---
+++ Semana04/Exercicio02/Video03/01Conceitos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01Conceitos
+{
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            
+            Person p1 = new Person();
+            p1.name = "Lizandro";
+            p1.age = 22;
+
+            p1.message();
+            
+        }
+    }
+}
